6f517cc74a1ac9628a9407f5fbe4bd7017fb711e
Leo Lafon <leo.lafon@epitech.eu>
2018-01-12 20:37:39 +0100

    added assets cache mechanics to app entry point

--- App.js
+++ App.js
@@ -1,8 +1,65 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * VPA - 2018
+ * leo.lafon@epitech.eu
+ */
 
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Asset, AppLoading } from 'expo'
+
+
+/**
+ * Caches images
+ *
+ * @param {*} images
+ */
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image)
+    } else {
+      return Asset.fromModule(image).downloadAsync()
+    }
+  })
+}
+
+
+/**
+ * App entry point
+ */
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isReady: false,
+    }
+  }
+
+  /**
+   * Asynchronously load assets
+   */
+  async loadAssetsAsync() {
+    const imageAssets = cacheImages([
+      require('./assets/vpa.png'),
+    ])
+
+    await Promise.all([
+      ...imageAssets,
+    ])
+  }
+
   render() {
+    // display splash screen while assets are loading
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this.loadAssetsAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      )
+    }
+
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
