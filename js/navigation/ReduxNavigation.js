581c406106fa4d4c984c93af225f4d52007332ec
Leo Lafon <leo.lafon@epitech.eu>
2018-01-13 16:21:05 +0100

    added support for android back button

--- js/navigation/ReduxNavigation.js
+++ js/navigation/ReduxNavigation.js
@@ -4,8 +4,12 @@
  */
 
 import React from 'react'
-import { addNavigationHelpers } from 'react-navigation'
 import { connect } from 'react-redux'
+import { BackHandler } from 'react-native'
+import {
+  addNavigationHelpers,
+  NavigationActions
+} from 'react-navigation'
 
 import RootNav from './AppNavigation'
 
@@ -14,14 +18,35 @@ import RootNav from './AppNavigation'
  *
  * @param {*} props
  */
-const ReduxNavigation = props => {
-  const { dispatch, nav } = props
-  const navigation = addNavigationHelpers({
-    dispatch,
-    state: nav,
-  })
+class ReduxNavigation extends React.Component {
+  componentDidMount() {
+    BackHandler.addEventListener(
+      'hardwareBackPress', this.onBackPress)
+  }
 
-  return <RootNav navigation={navigation}/>
+  componentWillUnmount() {
+    BackHandler.removeEventListener(
+      'hardwareBackPress', this.onBackPress)
+  }
+
+  onBackPress = () => {
+    const { dispatch, nav } = this.props
+    if (nav.index === 0) {
+      return false
+    }
+    dispatch(NavigationActions.back())
+    return true
+  }
+
+  render() {
+    const { dispatch, nav } = this.props
+    const navigation = addNavigationHelpers({
+      dispatch,
+      state: nav
+    })
+
+    return <RootNav navigation={navigation}/>
+  }
 }
 
 const mapStateToProps = state => ({ nav: state.nav })
